0eb0494cc0de7728dd13b1adf4b2094b5175d32a
qianlong-zhang <qianlong2006@gmail.com>
2019-03-10 20:46:21 +0800

    fix attachDebugger in zsim failed, because pintool child should be parent in pin_cmd.cpp

--- src/pin_cmd.cpp
+++ src/pin_cmd.cpp
@@ -50,14 +50,14 @@ PinCmd::PinCmd(Config* conf, const char* configFile, const char* outputDir, uint
     }
 
     args.push_back(pinPath);
-    args.push_back("-injection");
-    args.push_back("child");
-    args.push_back("-ifeellucky");
 
-    //Global pin options
-    args.push_back("-follow_execv"); //instrument child processes
-    args.push_back("-tool_exit_timeout"); //don't wait much of internal threads
-    args.push_back("1");
+	//Global pin options
+	args.push_back("-follow_execv"); //instrument child processes
+	args.push_back("-tool_exit_timeout"); //don't wait much of internal threads
+	args.push_back("1");
+	args.push_back("-ifeellucky");
+    args.push_back("-injection");
+    args.push_back("parent");
 
     //Additional options (e.g., -smc_strict for Java), parsed from config
     const char* pinOptions = conf->get<const char*>("sim.pinOptions", "");
