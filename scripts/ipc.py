4bef5a945655ae5ff958808bd54ab5b8bf9f70e1
qianlong-zhang <qianlong2006@gmail.com>
2019-03-12 10:04:45 +0800

    add function in ipc.py

--- scripts/ipc.py
+++ scripts/ipc.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 import h5py
+import math
 import numpy as np
+
 f = h5py.File('zsim-ev.h5', 'r')
 dset = f["stats"]["root"]
 stats = dset[-1]
@@ -8,6 +10,25 @@ phases = stats['phase']
 coreStats = stats['c']
 totalInstrs = coreStats['instrs']
 totalCycles = coreStats['cycles']
+olderr = np.seterr(all='ignore')
 ipc= (1. * totalInstrs)/totalCycles
 print "ipc = "
 print ipc
+
+arrLen = len(ipc)
+#print "arrLen = ", arrLen
+sum_ipc = 0
+i = 0
+for every_ipc in ipc:
+    if math.isnan(every_ipc):
+        continue
+    else:
+        #print "every_ipc = ", every_ipc
+        sum_ipc = sum_ipc + every_ipc
+        #print "sum_ipc = ", sum_ipc
+        i = i+1
+        #print "i = ", i
+print "Valid instances num = ", i
+print "Everage ipc = ", sum_ipc/i
+
+
